a8c29d222ddc02f743954a4c5630e26a49cc3581
tiago-mends <tiagomendesdev@gmail.com>
2024-07-16 19:04:11 -0300

    build(deps): update package-lock.json, package.json, App.tsx and card.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+
 export default function App() {
-	return <>aaaa</>;
+  const [pokemons, setPokemons] = useState<AxiosResponse[]>([]);
+
+  const fetchPokemons = () => {
+    const pokemonEndpoints = [];
+    for (let i = 1; i < 10000; i++) {
+      pokemonEndpoints.push("https://pokeapi.co/api/v2/pokemon/" + i);
+    }
+    axios
+      .all(pokemonEndpoints.map((endpoint) => axios.get(endpoint)))
+      .then((res) => setPokemons(res));
+  };
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  return (
+    <div>
+      {/* {pokemons.map((pokemon, key) => (
+        <div key={key}>{pokemon.data.name}</div>
+      ))} */}
+    </div>
+  );
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,3 +1,9 @@
-export default function Card() {
-  return <div>Card</div>;
+import { ReactNode } from "react";
+
+interface CardProps {
+  children: ReactNode;
+}
+
+export default function Card({ children }: CardProps) {
+  return <div>{children}</div>;
 }
