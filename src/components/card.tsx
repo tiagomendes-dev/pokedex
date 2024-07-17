fe0e4ab32dbe2a5eb88578eb1be4cb6f0346c2a3
tiago-mends <tiagomendesdev@gmail.com>
2024-07-17 12:20:41 -0300

    update card.tsx and main-layout.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,9 +6,9 @@ interface CardProps {
 
 export default function Card({ id, image, name }: CardProps) {
   return (
-    <div className="cursor-pointer rounded-lg bg-neutral-100 p-4 shadow transition hover:scale-105 hover:shadow-md">
+    <div className="group cursor-pointer rounded-lg bg-neutral-100 p-4 shadow transition hover:scale-105 hover:bg-neutral-200 hover:shadow-md">
       <div className="flex items-center">
-        <div className="relative rounded-xl bg-zinc-200 p-4">
+        <div className="relative rounded-xl bg-zinc-200 p-4 transition group-hover:bg-zinc-300">
           <img src={image} alt={name} className="size-20" />
           <span className="absolute bottom-2 left-2 rounded-full bg-zinc-50 px-2 text-xs text-black/60">
             #{id.toString().padStart(3, "0")}
--- src/main-layout.tsx
+++ src/main-layout.tsx
@@ -11,7 +11,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <>
       <Header />
-      <main className="min-h-[calc(100vh-18.25rem)]">{children}</main>
+      <main className="min-h-[calc(100vh-20.73rem)]">{children}</main>
       <Footer />
     </>
   );
