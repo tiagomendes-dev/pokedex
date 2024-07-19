a5ad21a1fd449a7cd609bdfe400bd1fa454847f6
tiago-mends <tiagomendesdev@gmail.com>
2024-07-19 01:46:34 -0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ export default function App() {
               id: pokemonDetail.data.id,
               name: pokemon.name,
               url: pokemon.url,
-              imageUrl: pokemonDetail.data.sprites.other.showdown.front_default,
+              // imageUrl: pokemonDetail.data.sprites.other.showdown.front_default,
             };
           }),
         );
